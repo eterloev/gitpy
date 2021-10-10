7bd7fa09968e57f223aa11292e79116396d43a0a
Emil <emil199@yandex.ru>
2021-10-10 15:18:07 +0300

    Implemented a random forest classifier

--- forest.py
+++ forest.py
@@ -1,58 +1,39 @@
-from __future__ import division, print_function
-# отключим всякие предупреждения Anaconda
-import warnings
-warnings.filterwarnings('ignore')
-
-import seaborn as sns
-# russian headres
-from matplotlib import rc
-font = {'family': 'Verdana',
-        'weight': 'normal'}
-rc('font', **font)
 import pandas as pd
 import numpy as np
-from sklearn.ensemble.forest import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import metrics
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.preprocessing import StandardScaler
 
-hostel_data = pd.read_csv("creditcard.csv")
-features = {"f1":u"Персонал",
-"f2":u"Бронирование хостела ",
-"f3":u"Заезд в хостел и выезд из хостела",
-"f4":u"Состояние комнаты",
-"f5":u"Состояние общей кухни",
-"f6":u"Состояние общего пространства",
-"f7":u"Дополнительные услуги",
-"f8":u"Общие условия и удобства",
-"f9":u"Цена/качество",
-"f10":u"ССЦ"}
+dataset = pd.read_csv('creditcard.csv')
 
-forest = RandomForestRegressor(n_estimators=1000, max_features=10,
-                                random_state=0)
 
-forest.fit(hostel_data.drop(['hostel', 'Class'], axis=1), 
-           hostel_data['Class'])
-importances = forest.feature_importances_
+X = dataset.iloc[:, 0:30].values
+y = dataset.iloc[:, 30].values
 
-indices = np.argsort(importances)[::-1]
-# Plot the feature importancies of the forest
-num_to_plot = 10
-feature_indices = [ind+1 for ind in indices[:num_to_plot]]
+print("Enter the testing size")
+inp = -1.0
 
-# Print the feature ranking
-print("Feature ranking:")
+while(inp <= 0 or inp >= 100.0 or inp == 1.0):
+    inp = float(input("the size must be positive and less than 100: "))
+    if (inp > 1.0 and inp/100.0 < 1.0):
+        print("assuming percentages")
+        inp /= 100.0
 
-for f in range(num_to_plot):
-    print("%d. %s %f " % (f + 1, 
-            features["f"+str(feature_indices[f])], 
-            importances[indices[f]]))
-plt.figure(figsize=(15,5))
-plt.title(u"Важность конструктов")
-bars = plt.bar(range(num_to_plot), 
-               importances[indices[:num_to_plot]],
-       color=([str(i/float(num_to_plot+1)) 
-               for i in range(num_to_plot)]),
-               align="center")
-ticks = plt.xticks(range(num_to_plot), 
-                   feature_indices)
-plt.xlim([-1, num_to_plot])
-plt.legend(bars, [u''.join(features["f"+str(i)]) 
-                  for i in feature_indices]);
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=inp, random_state=0)
+
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+regressor = RandomForestClassifier(n_estimators=100, random_state=0)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+
+
+print(classification_report(y_test,y_pred))
+print("Accuracy: ")
+print(accuracy_score(y_test, y_pred))
+
+
